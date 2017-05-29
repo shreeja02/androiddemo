5ef3dc7a131100fd9252e4e8a15d96e5715649a5
shreeja02 <shreeja.shah97@gmail.com>
2017-05-29 13:46:10 +0530

    done

--- app/src/main/java/com/example/dell/myapplication/AllCollectionActivity.java
+++ app/src/main/java/com/example/dell/myapplication/AllCollectionActivity.java
@@ -4,6 +4,9 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -17,22 +20,101 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class AllCollectionActivity extends AppCompatActivity {
-    GridView collectionlst;
+    ListView collectionlst;
     ArrayList<collection> mArrayList;
+    AsyncHttpClient mClient;
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+       // MenuInflater inflat=getMenuInflater();
+//        inflat.inflate();
+        //getMenuInflater().inflate();
+        menu.setHeaderTitle("Select the action");
+        menu.add("Delete");
+        menu.add("Edit");
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info= (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final collection selectedItem=mArrayList.get(info.position);
+        mClient=new AsyncHttpClient();
+
+        if(item.getTitle()=="Delete"){
+
+            JSONObject mJsonObject=new JSONObject();
+            StringEntity entity=null;
+            try {
+
+                mJsonObject.put("description", "Inspired by science fictional depictions of arid worlds and the cultural practices that develop around water scarcity, this piece envisions a propositional social ritual that has developed in response to the growing threat of water scarcity in California. Through a combination of diegetic prototypes");
+                entity=new StringEntity(mJsonObject.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            mClient.delete("https://reunirbackend.herokuapp.com/collection/"+selectedItem.getColl_id(),new JsonHttpResponseHandler(){
+
+                ProgressDialog mProgress;
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                    super.onSuccess(statusCode, headers, response);
+                    mArrayList.remove(selectedItem);
+                    collectionlst.setAdapter(new collection_custom_adapter(AllCollectionActivity.this,mArrayList));
+                   // Toast.makeText(AllCollectionActivity.this,"Deleted",Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                    super.onFailure(statusCode, headers, throwable, errorResponse);
+                }
+
+                @Override
+                public void onStart() {
+                    super.onStart();
+                    mProgress=ProgressDialog.show(AllCollectionActivity.this,"Loading","Please Wait",true,false);
+
+                }
+
+                @Override
+                public void onFinish() {
+                    super.onFinish();
+                    if(mProgress.isShowing()){
+                        mProgress.dismiss();
+                    }}
+
+            });
+            //mClient.post(LoginActivity.this,"https://reunirbackend.herokuapp.com/collection",entity,"application/json",new JsonHttpResponseHandler(){
+
+            Toast.makeText(AllCollectionActivity.this,selectedItem.getColl_name(),Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(AllCollectionActivity.this,"Edit",Toast.LENGTH_LONG).show();
+        }
+        return super.onContextItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_collection);
-        collectionlst= (GridView) findViewById(R.id.listcollection);
+        collectionlst= (ListView) findViewById(R.id.listcollection);
         mArrayList=new ArrayList<collection>();
 
-        AsyncHttpClient mClient=new AsyncHttpClient();
+        mClient=new AsyncHttpClient();
         mClient.get(AllCollectionActivity.this,"https://reunirbackend.herokuapp.com/collection/",new JsonHttpResponseHandler(){
 
             ProgressDialog mProgress;
@@ -53,8 +135,11 @@ public class AllCollectionActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 collectionlst.setAdapter(new collection_custom_adapter(AllCollectionActivity.this,mArrayList));
+               registerForContextMenu(collectionlst);
+
 
             }
+
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
@@ -76,6 +161,7 @@ public class AllCollectionActivity extends AppCompatActivity {
                 }
             }
         });
+
         collectionlst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
