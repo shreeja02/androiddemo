8ef821520208844c868a2ba151d0e2618a03a194
shreeja02 <shreeja.shah97@gmail.com>
2017-06-02 13:01:00 +0530

    menus

--- app/src/main/java/com/example/dell/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/dell/myapplication/Main2Activity.java
@@ -3,18 +3,46 @@ package com.example.dell.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
     Button btncalc,btnlogin,btnsignup,btnStudent,btnCollection,btnStrAdapter;
     EditText et1;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menudemo,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuAdd:
+                Toast.makeText(Main2Activity.this,"Add",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.help:
+                Toast.makeText(Main2Activity.this,"Help",Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+
         btncalc= (Button) findViewById(R.id.btnmain2calc);
         btnlogin= (Button) findViewById(R.id.btnmain2login);
         btnsignup= (Button) findViewById(R.id.btnmain2signup);
--- app/src/main/java/com/example/dell/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.dell.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,6 +50,30 @@ public class MainActivity extends AppCompatActivity {
            btnsqrt.setVisibility(view.INVISIBLE);
        }
    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu2,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menudelete:
+                Toast.makeText(MainActivity.this,"delete",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.menuedit:
+                Toast.makeText(MainActivity.this,"edit",Toast.LENGTH_LONG).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
